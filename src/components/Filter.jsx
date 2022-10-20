eed8848a6ca67daaba317c82491372c0f70718d3
André Melo <proandremelo@gmail.com>
2022-10-20 19:30:14 -0300

    Requisito 4 arrumado

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -29,6 +29,7 @@ function Filter() {
 
   const handleFilterChange = ({ target: { name, value } }) => {
     setActualFilter({ ...actualFilter, [name]: value });
+    console.log(actualFilter);
   };
 
   const createOrigin = () => {
@@ -45,6 +46,11 @@ function Filter() {
 
   const saveFilter = () => {
     setFilters([...filters, actualFilter]);
+    const result = COLUMN.filter((col) => {
+      const cols = filters.map((filt) => filt.column);
+      return !cols.includes(col);
+    });
+    setActualFilter({ ...actualFilter, column: result[1] });
   };
 
   const filterSwitch = (origin) => {
@@ -66,10 +72,31 @@ function Filter() {
     }
   };
 
+  // const filterSwitch = (origin) => {
+  //   console.log('origin', origin);
+  //   const result = [];
+  //   filters
+  // };
+
   const handleFilterClick = () => {
     const origin = createOrigin();
     filterSwitch(origin);
     saveFilter();
+  };
+
+  const removeFilter = ({ target }) => {
+    setFilters(filters.filter((filt) => filt.column !== target.name));
+    setActualFilter();
+  };
+
+  const removeAllFilters = () => {
+    setFilters([]);
+    setFilteredPlanets([]);
+    setActualFilter({
+      column: COLUMN[0],
+      comparison: COMPARISON[0],
+      value: 0,
+    });
   };
 
   return (
@@ -81,39 +108,71 @@ function Filter() {
           onChange={ handleFilterByName }
         />
       </section>
-      <select
-        name="column"
-        value={ actualFilter.column }
-        data-testid="column-filter"
-        onChange={ handleFilterChange }
-      >
-        {COLUMN.filter((col) => {
-          const cols = filters.map((filt) => filt.column);
-          return !cols.includes(col);
-        }).map((option) => <option key={ option }>{ option }</option>)}
-      </select>
-      <select
-        name="comparison"
-        value={ actualFilter.comparison }
-        data-testid="comparison-filter"
-        onChange={ handleFilterChange }
-      >
-        {COMPARISON.map((option) => <option key={ option }>{ option }</option>)}
-      </select>
-      <input
-        name="value"
-        type="number"
-        value={ actualFilter.value }
-        data-testid="value-filter"
-        onChange={ handleFilterChange }
-      />
-      <button
-        type="button"
-        data-testid="button-filter"
-        onClick={ handleFilterClick }
-      >
-        Filtrar
-      </button>
+      <section>
+        <select
+          name="column"
+          value={ actualFilter.column }
+          data-testid="column-filter"
+          onChange={ handleFilterChange }
+        >
+          {COLUMN.filter((col) => {
+            const cols = filters.map((filt) => filt.column);
+            return !cols.includes(col);
+          }).map((option) => (
+            <option
+              key={ option }
+            >
+              { option }
+            </option>
+          ))}
+        </select>
+        <select
+          name="comparison"
+          value={ actualFilter.comparison }
+          data-testid="comparison-filter"
+          onChange={ handleFilterChange }
+        >
+          {COMPARISON.map((option) => <option key={ option }>{ option }</option>)}
+        </select>
+        <input
+          name="value"
+          type="number"
+          value={ actualFilter.value }
+          data-testid="value-filter"
+          onChange={ handleFilterChange }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ handleFilterClick }
+        >
+          Filtrar
+        </button>
+        <button
+          type="button"
+          data-testid="button-remove-filters"
+          onClick={ removeAllFilters }
+        >
+          Remover Filtros
+        </button>
+      </section>
+      <section>
+        {
+          filters.map((filt, index) => (
+            <div key={ index }>
+              {`${filt.column} ${filt.comparison} ${filt.value}` }
+              <button
+                type="button"
+                name={ filt.column }
+                data-testid="filter"
+                onClick={ removeFilter }
+              >
+                Remover
+              </button>
+            </div>
+          ))
+        }
+      </section>
     </section>
   );
 }
